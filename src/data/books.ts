83f00de2e3767da1a6921052bdaa3e48e050f927
Benjamim Matos <benjamim.matos@gmail.com>
2025-09-15 01:29:17 +0000

    corrigir o arquivo src/data/books.ts

---
+++ src/data/books.ts
@@ -0,0 +1,267 @@
+
+export const bibleBooksARC: { id: number; name: string }[] = [
+    {
+      "id": 31419,
+      "name": "1 Coríntios"
+    },
+    {
+      "id": 31386,
+      "name": "1 Crônicas"
+    },
+    {
+      "id": 31435,
+      "name": "1 João"
+    },
+    {
+      "id": 31433,
+      "name": "1 Pedro"
+    },
+    {
+      "id": 31384,
+      "name": "1 Reis"
+    },
+    {
+      "id": 31382,
+      "name": "1 Samuel"
+    },
+    {
+      "id": 31425,
+      "name": "1 Tessalonicenses"
+    },
+    {
+      "id": 31427,
+      "name": "1 Timóteo"
+    },
+    {
+      "id": 31420,
+      "name": "2 Coríntios"
+    },
+    {
+      "id": 31387,
+      "name": "2 Crônicas"
+    },
+    {
+      "id": 31436,
+      "name": "2 João"
+    },
+    {
+      "id": 31434,
+      "name": "2 Pedro"
+    },
+    {
+      "id": 31385,
+      "name": "2 Reis"
+    },
+    {
+      "id": 31383,
+      "name": "2 Samuel"
+    },
+    {
+      "id": 31426,
+      "name": "2 Tessalonicenses"
+    },
+    {
+      "id": 31428,
+      "name": "2 Timóteo"
+    },
+    {
+      "id": 31437,
+      "name": "3 João"
+    },
+    {
+      "id": 31410,
+      "name": "Ageu"
+    },
+    {
+      "id": 31403,
+      "name": "Amós"
+    },
+    {
+      "id": 31439,
+      "name": "Apocalipse"
+    },
+    {
+      "id": 31417,
+      "name": "Atos"
+    },
+    {
+      "id": 31395,
+      "name": "Cânticos"
+    },
+    {
+      "id": 31424,
+      "name": "Colossenses"
+    },
+    {
+      "id": 31400,
+      "name": "Daniel"
+    },
+    {
+      "id": 31378,
+      "name": "Deuteronômio"
+    },
+    {
+      "id": 31394,
+      "name": "Eclesiastes"
+    },
+    {
+      "id": 31422,
+      "name": "Efésios"
+    },
+    {
+      "id": 31388,
+      "name": "Esdras"
+    },
+    {
+      "id": 31390,
+      "name": "Ester"
+    },
+    {
+      "id": 31375,
+      "name": "Êxodo"
+    },
+    {
+      "id": 31399,
+      "name": "Ezequiel"
+    },
+    {
+      "id": 31430,
+      "name": "Filemom"
+    },
+    {
+      "id": 31423,
+      "name": "Filipenses"
+    },
+    {
+      "id": 31421,
+      "name": "Gálatas"
+    },
+    {
+      "id": 31374,
+      "name": "Gênesis"
+    },
+    {
+      "id": 31408,
+      "name": "Habacuque"
+    },
+    {
+      "id": 31431,
+      "name": "Hebreus"
+    },
+    {
+      "id": 31396,
+      "name": "Isaías"
+    },
+    {
+      "id": 31397,
+      "name": "Jeremias"
+    },
+    {
+      "id": 31391,
+      "name": "Jó"
+    },
+    {
+      "id": 31416,
+      "name": "João"
+    },
+    {
+      "id": 31402,
+      "name": "Joel"
+    },
+    {
+      "id": 31405,
+      "name": "Jonas"
+    },
+    {
+      "id": 31379,
+      "name": "Josué"
+    },
+    {
+      "id": 31438,
+      "name": "Judas"
+    },
+    {
+      "id": 31380,
+      "name": "Juízes"
+    },
+    {
+      "id": 31398,
+      "name": "Lamentações"
+    },
+    {
+      "id": 31376,
+      "name": "Levítico"
+    },
+    {
+      "id": 31415,
+      "name": "Lucas"
+    },
+    {
+      "id": 31412,
+      "name": "Malaquias"
+    },
+    {
+      "id": 31414,
+      "name": "Marcos"
+    },
+    {
+      "id": 31413,
+      "name": "Mateus"
+    },
+    {
+      "id": 31406,
+      "name": "Miquéias"
+    },
+    {
+      "id": 31407,
+      "name": "Naum"
+    },
+    {
+      "id": 31389,
+      "name": "Neemias"
+    },
+    {
+      "id": 31377,
+      "name": "Números"
+    },
+    {
+      "id": 31404,
+      "name": "Obadias"
+    },
+    {
+      "id": 31401,
+      "name": "Oséias"
+    },
+    {
+      "id": 31393,
+      "name": "Provérbios"
+    },
+    {
+      "id": 31418,
+      "name": "Romanos"
+    },
+    {
+      "id": 31381,
+      "name": "Rute"
+    },
+    {
+      "id": 31392,
+      "name": "Salmos"
+    },
+    {
+      "id": 31409,
+      "name": "Sofonias"
+    },
+    {
+      "id": 31432,
+      "name": "Tiago"
+    },
+    {
+      "id": 31429,
+      "name": "Tito"
+    },
+    {
+      "id": 31411,
+      "name": "Zacarias"
+    }
+  ];
